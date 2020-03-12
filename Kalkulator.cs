ea97bb6e45f6ff506554ea5237d0720798068f7b
nurazmiaprilia <nurazmi.aprilia@students.amikom.ac.id>
2020-03-12 12:49:41 +0700

    project kalkulator baru

---
+++ Kalkulator.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+		
+		static int Penambahan(int a, int b)
+		{
+			return a + b;
+		}
+		
+		static int Pengurangan(int a, int b)
+		{
+			return a - b;
+		}
+		
+		// TODO: tambahkan method untuk Perkalian dan Pembagian
+		
+		static int Perkalian(int a, int b)
+		{
+			return a * b;
+		}
+		
+		static int Pembagian(int a, int b)
+		{
+			return a / b;
+		}
+	}
+}
